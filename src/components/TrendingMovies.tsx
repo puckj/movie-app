19a1cb11d99e468d262d85e1f95f7c57bb93715c
Pongsapuck_Dev <pongsapuck.j@gmail.com>
2023-06-20 15:14:02 +0700

    create TrendingMovies components and patch react-native-snap-carousel module

---
+++ src/components/TrendingMovies.tsx
@@ -0,0 +1,30 @@
+import { View, Text, TouchableWithoutFeedback } from "react-native";
+import React from "react";
+import Carousel from "react-native-snap-carousel";
+
+const TrendingMovies = ({ data }) => {
+  return (
+    <View className="mb-8">
+      <Text className="text-white text-xl mx-4 mb-5">Trending</Text>
+      <Carousel
+        data={data}
+        renderItem={({ item }) => <MovieCard item={item} />}
+        firstItem={1}
+        inactiveSlideOpacity={0.60}
+        sliderWidth={600}
+        itemWidth={400}
+        slideStyle={{display:'flex', alignItems: 'center'}}
+      />
+    </View>
+  );
+};
+
+export default TrendingMovies;
+
+const MovieCard = ({ item }) => {
+  return (
+    <TouchableWithoutFeedback>
+      <Text>Movie</Text>
+    </TouchableWithoutFeedback>
+  );
+};
--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,15 +1,44 @@
-import { View, Text, Platform } from "react-native";
-import React from "react";
+import {
+  View,
+  Text,
+  Platform,
+  TouchableOpacity,
+  ScrollView,
+} from "react-native";
+import React, { useState } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { StatusBar } from "expo-status-bar";
+import {
+  Bars3CenterLeftIcon,
+  MagnifyingGlassIcon,
+} from "react-native-heroicons/outline";
+import { styles } from "../theme";
+import TrendingMovies from "../components/TrendingMovies";
+
 const ios = Platform.OS == "ios";
 const HomeScreen = (): JSX.Element => {
+  const [trending, setTrending] = useState([1, 2, 3]);
   return (
     <View className="flex-1 bg-neutral-800">
       {/* search bar and log */}
       <SafeAreaView className={ios ? "-mb-2" : "mb-3"}>
         <StatusBar style="light" />
+        <View className="flex-row justify-between items-center mx-4">
+          <Bars3CenterLeftIcon size="30" strokeWidth={2} color="white" />
+          <Text className="text-white text-3xl font-bold">
+            <Text style={styles.text}>M</Text>ovies
+          </Text>
+          <TouchableOpacity>
+            <MagnifyingGlassIcon size={30} strokeWidth={2} color="white" />
+          </TouchableOpacity>
+        </View>
       </SafeAreaView>
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ paddingBottom: 10 }}
+      >
+        <TrendingMovies data={trending} />
+      </ScrollView>
     </View>
   );
 };
